294d39cee0acac4b9cf04d30dbeff0514a918dbc
Daniel Oliveira <devdoliveiradd@gmail.com>
2022-11-05 00:02:04 -0300

    Jokenpo

---
+++ assets/js/script.js
@@ -0,0 +1,144 @@
+/* Score Gaming */
+const scorePlayer = document.getElementById("youScore");
+const scoreCpu = document.getElementById("cpuScore");
+const winsRound = document.getElementById("winsRound");
+let playerScore = 0;
+
+/* Options Player */
+const playerRock = document.getElementById("playerRock");
+const playerPaper = document.getElementById("playerPaper");
+const playerScissors = document.getElementById("playerScissors");
+
+/* Options CPU */
+const cpuRock = document.getElementById("cpuRock");
+const cpuPaper = document.getElementById("cpuPaper");
+const cpuScissors = document.getElementById("cpuScissors");
+
+const game = () => {
+  playerRock.addEventListener("click", () => {
+    playerRock.classList.add("active");
+    playerPaper.classList.remove("active");
+    playerScissors.classList.remove("active");
+    cpuOptions();
+  });
+  playerPaper.addEventListener("click", () => {
+    playerRock.classList.remove("active");
+    playerPaper.classList.add("active");
+    playerScissors.classList.remove("active");
+    cpuOptions();
+  });
+  playerScissors.addEventListener("click", () => {
+    playerRock.classList.remove("active");
+    playerPaper.classList.remove("active");
+    playerScissors.classList.add("active");
+    cpuOptions();
+  });
+};
+
+/* Opções da CPU */
+const cpuOptions = () => {
+  const options = ["rock", "paper", "scissors"];
+  const cpuNumber = Math.floor(Math.random() * 3);
+  const cpuChoice = options[cpuNumber];
+  switch (cpuChoice) {
+    case "rock":
+      cpuRock.classList.add("active");
+      cpuPaper.classList.remove("active");
+      cpuScissors.classList.remove("active");
+      break;
+    case "paper":
+      cpuRock.classList.remove("active");
+      cpuPaper.classList.add("active");
+      cpuScissors.classList.remove("active");
+      break;
+    case "scissors":
+      cpuRock.classList.remove("active");
+      cpuPaper.classList.remove("active");
+      cpuScissors.classList.add("active");
+      break;
+  }
+  checkWinner();
+};
+
+/* Verifica o vencedor seja CPU ou Player e atualiza o placa de pontuação */
+const checkWinner = () => {
+  if (
+    playerRock.classList.contains("active") &&
+    cpuRock.classList.contains("active")
+  ) {
+    winsRound.innerHTML = "Game Tied";
+  } else if (
+    playerRock.classList.contains("active") &&
+    cpuPaper.classList.contains("active")
+  ) {
+    winsRound.innerHTML = "CPU Wins!";
+    playerScore--;
+    scoreCpu.innerHTML = `CPU Score!: ${playerScore}`;
+  } else if (
+    playerRock.classList.contains("active") &&
+    cpuScissors.classList.contains("active")
+  ) {
+    winsRound.innerHTML = "You Wins!";
+    playerScore++;
+    scorePlayer.innerHTML = `Your Score: ${playerScore}`;
+  } else if (
+    playerPaper.classList.contains("active") &&
+    cpuRock.classList.contains("active")
+  ) {
+    winsRound.innerHTML = "You Wins!";
+    playerScore++;
+    scorePlayer.innerHTML = `Your Score: ${playerScore}`;
+  } else if (
+    playerPaper.classList.contains("active") &&
+    cpuPaper.classList.contains("active")
+  ) {
+    winsRound.innerHTML = "Game Tied";
+  } else if (
+    playerPaper.classList.contains("active") &&
+    cpuScissors.classList.contains("active")
+  ) {
+    winsRound.innerHTML = "CPU Wins!";
+    playerScore--;
+    scoreCpu.innerHTML = `CPU Score: ${playerScore}`;
+  } else if (
+    playerScissors.classList.contains("active") &&
+    cpuRock.classList.contains("active")
+  ) {
+    winsRound.innerHTML = "CPU Wins!";
+    playerScore--;
+    scoreCpu.innerHTML = `CPU Score: ${playerScore}`;
+  } else if (
+    playerScissors.classList.contains("active") &&
+    cpuPaper.classList.contains("active")
+  ) {
+    winsRound.innerHTML = "You Wins!";
+    playerScore++;
+    scorePlayer.innerHTML = `Your Score: ${playerScore}`;
+  } else if (
+    playerScissors.classList.contains("active") &&
+    cpuScissors.classList.contains("active")
+  ) {
+    winsRound.innerHTML = "Game Tied";
+  }
+};
+
+game();
+
+/* Theme */
+const latterTheme = document.getElementById("latterTheme");
+const proxTheme = document.getElementById("nextTheme");
+const containerScreen = document.getElementById("screen");
+
+/* Theme */
+proxTheme.addEventListener("click", () => {
+  containerScreen.style.backgroundImage = "url(assets/img/background-1.png)";
+  proxTheme.style.cursor = "not-allowed";
+  latterTheme.style.cursor = "pointer";
+});
+
+/* Theme */
+latterTheme.addEventListener("click", () => {
+  containerScreen.style.backgroundImage = "url(assets/img/background-2.png)";
+  latterTheme.style.cursor = "not-allowed";
+  proxTheme.style.cursor = "pointer";
+});
---
+++ readme.md
@@ -0,0 +1,29 @@
+<img src="./assets/img/capa-readme.png"/>
+
+#
+
+<br>
+<h3 align="center">🎮 História do Projeto</h3>
+<br>
+
+<p align="center">Desenvolvi este projeto após relembrar da minha infancia, da época que eu jogava no Nintendo... Eu iniciei com wireframes fazendo toda a prototipação e ai parti para o Figma e criei o layout. Após terminar iniciei o desenvolvimento. Utilizando somente: HTML, CSS e JS!</p>
+
+<br>
+
+<h3 align="center">O que foi utilizado 👇🏻</h3>
+
+<br>
+
+<p align="center">
+  <a href="https://skillicons.dev">
+    <img src="https://skillicons.dev/icons?i=html,css,js,figma" width="130px"/>
+  </a>
+</p>
+
+<br>
+<img src="./assets/img/capa-projeto.png" />
+<br>
+
+<br>
+<p align="center"><a href="https://game-jokenpo.surge.sh/"><b>Clique aqui e acesse o jogo</b></a> 
+</p>
